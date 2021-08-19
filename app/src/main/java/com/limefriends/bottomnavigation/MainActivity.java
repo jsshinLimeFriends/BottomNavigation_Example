392db9ca6cb46aa108644b74d0cb8cc30f0e79c1
DESKTOP-15BH2CF\limefriends <jsshin@limefriends.com>
2021-08-19 17:54:57 +0900

    Bottom Navigation View Example

---
+++ app/src/main/java/com/limefriends/bottomnavigation/MainActivity.java
@@ -0,0 +1,18 @@
+package com.limefriends.bottomnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navigationView = findViewById(R.id.navigation);
+        navigationView.setItemIconTintList(null);
+    }
+}
